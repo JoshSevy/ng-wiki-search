aeb118dd474bbe56996dbf1bab52d2dd810bf06f
JoshSevy <joshuasevy@outlook.com>
2021-01-18 19:01:35 -0700

    Successful communication between wiki service and app comp

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -4,6 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WikipediaService {
+  url: string = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=space';
 
   constructor() { }
+
+  public search(term: string)  {
+    return 'I am wikipedia search results';
+  }
 }
